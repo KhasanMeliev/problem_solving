92725ef65427efdf3905f51dcc963cf4461faa26
KhasanMeliev <khasanmeliev2@gmail.com>
2022-04-27 21:45:55 +0500

    day4

---
+++ solve.js
@@ -0,0 +1,19 @@
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function randomItem(arr) {
+//   let random = arr[Math.floor(Math.random() * arr.length)];
+//   return random;
+// }
+// console.log(randomItem(array));
+
+// let array1 = [1, 2, 3, 4];
+// let array2 = [1, 2, 3];
+// function compareArrays(arr1, arr2) {
+//   let arr1Elements = JSON.stringify(arr1)
+//   let arr2Elements = JSON.stringify(arr2)
+//       if (arr1Elements === arr2Elements) {
+//         return `bu arraylar teng`;
+//       } else {
+//         return `bu arraylar teng emas`;
+//       }
+// }
+// console.log(compareArrays(array1, array2));
