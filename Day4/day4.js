3d2a157219f03f1febdbb9cab808cae08f33651a
KhasanMeliev <khasanmeliev2@gmail.com>
2022-04-27 21:49:28 +0500

    day4

--- Day4/day4.js
+++ Day4/day4.js
@@ -33,6 +33,22 @@
 //   }
 // }
 // console.log(compareArrays(array1, array2));
+// let array1 = [1, 2, 3, 4, 5];
+// let array2 = [1, 2, 3, 4, 5];
+// function compareArrays(arr1, arr2) {
+//   if (arr1.length == arr2.length) {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] == arr2[i]) {
+//         return "arraylar teng";
+//       } else {
+//         return "arraylar teng emas";
+//       }
+//     }
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(compareArrays(array1, array2));
 
 //----------------------------Get random Item fron an Array-------------------
 // let array = [1, 2, 3, 4, 5, 6, 7, 8];
